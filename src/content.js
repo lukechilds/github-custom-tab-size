f3d90e457cedf97e5c2a6fae9c4e4c8003e70dad
Luke Childs <lukechilds123@gmail.com>
2017-08-12 13:06:25 +0100

    Update CSS to overwrite refined-github

--- src/content.js
+++ src/content.js
@@ -4,14 +4,9 @@ document.addEventListener('DOMContentLoaded', () => document.body.appendChild(st
 
 // Set tab size styles
 const setTabSizeStyles = size => style.innerHTML = `
-.tab-size[data-tab-size='2'],
-.tab-size[data-tab-size='4'],
-.tab-size[data-tab-size='8'],
-.inline-review-comment,
-.gist table.lines,
-table.diff-table,
-.markdown-body pre {
-	tab-size: ${size} !important;
+* {
+	-moz-tab-size: ${size} !important;
+	     tab-size: ${size} !important;
 }`;
 
 // Set tab size from settings on load
